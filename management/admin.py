52667e4447fab29a19ae42b70ecbc8fdcffd930b
Laurent Ouma <lawiomosh3@gmail.com>
2023-06-12 07:46:34 +0300

    added management models to the admin

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import BusinessStaff, StaffPermission
+
+
 # Register your models here.
+
+@admin.register(BusinessStaff)
+class BusinessStaffAdmin(admin.ModelAdmin):
+    list_display = ('branch', 'user', 'title', 'created_at', 'updated_at')
+
+
+@admin.register(StaffPermission)
+class StaffPermissionAdmin(admin.ModelAdmin):
+    list_display = ('staff', 'write_business', 'write_branch', 'write_product')
