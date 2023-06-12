394b43d162e9ed12ea0161434d832cba52fb86af
Laurent Ouma <lawiomosh3@gmail.com>
2023-06-12 09:09:48 +0300

    Added businesses viewset however still using the ids and not slugs

--- businesses/views.py
+++ businesses/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import BusinessCategory, BusinessCatalog, BusinessBranch
+from .serializers import BusinessCategorySerializer, BusinessCatalogSerializer, BusinessBranchSerializer
+
 
 # Create your views here.
+
+
+class BusinessCategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = BusinessCategorySerializer
+    queryset = BusinessCategory.objects.all()
+
+
+class BusinessCatalogViewSet(viewsets.ModelViewSet):
+    serializer_class = BusinessCatalogSerializer
+    queryset = BusinessCatalog.objects.filter(is_approved=True)
+
+
+class BusinessBranchViewSet(viewsets.ModelViewSet):
+    serializer_class = BusinessBranchSerializer
+    queryset = BusinessBranch.objects.filter(is_approved=True)
